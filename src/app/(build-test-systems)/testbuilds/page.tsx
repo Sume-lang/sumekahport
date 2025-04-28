b35bbd276da352a84e7af62bec6ac3e9ec065e15
Advsum <sumekah33@gmail.com>
2025-04-28 09:18:44 +0800

    feat: add Navigations and Footers components with responsive design

---
+++ src/app/(build-test-systems)/testbuilds/page.tsx
@@ -0,0 +1,7 @@
+export default function TestBuildSystems() {
+  return (
+    <main className="flex flex-col items-center justify-center w-full h-screen">
+      <section>Test Build-Systems</section>
+    </main>
+  );
+}
