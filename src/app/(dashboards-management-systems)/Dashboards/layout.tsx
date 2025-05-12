17f57038847ca3db1d9857fb2d6368e1f5185178
Advsum <sumekah33@gmail.com>
2025-05-13 00:08:52 +0800

    feat: Add Sidebar component with navigation links and responsive design

--- src/app/(dashboards-management-systems)/Dashboards/layout.tsx
+++ src/app/(dashboards-management-systems)/Dashboards/layout.tsx
@@ -3,9 +3,8 @@ const lato = Lato({
   subsets: ["latin"],
   weight: ["100", "300", "400", "700", "900"],
 });
-import Navigations from "../allcomponents/UI/Navigations";
-import Footer from "../allcomponents/UI/footers";
 import { Metadata } from "next";
+import { SidebarDemo } from "../allcomponents/UI/sidebar";
 
 export const metadata: Metadata = {
   title: "Ahmad Gustiawan Anton Sumekah",
@@ -18,20 +17,12 @@ export default function MyPageLayout({
 }>) {
   return (
     <main
-      className={`${lato.className}flex flex-col min-h-screen bg-gradient-to-br from-[#20202e] to-[#2d2d32] text-[#faad86]`}
+      className={`${lato.className} flex lg:flex-row flex-col w-full h-screen bg-gradient-to-br from-[#20202e] to-[#2d2d32] text-[#faad86]`}
     >
-      <section>
-        <Navigations />
+      <section className="lg:w-[15vw] md:w-[50%]">
+        <SidebarDemo />
       </section>
-      <section className="min-h-screen mb-20">{children}</section>
-      <section className="">
-        <Footer />
-        <div className="flex lg:flex-row items-center justify-center flex-col text-[12px] text-center font-light w-full p-2 lg:gap-5 bg-[#faad86]/20">
-          <h1 className="">@copyright 2025 - Ahmad Gustiawan Anton Sumekah</h1>
-          <p>-</p>
-          <h1>privacy policy | terms of service | contact</h1>
-        </div>
-      </section>
+      <section className="h-screen w-full">{children}</section>
     </main>
   );
 }
