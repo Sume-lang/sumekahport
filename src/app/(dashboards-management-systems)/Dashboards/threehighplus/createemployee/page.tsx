67848d82f775100a32fe608dea643cd571911db0
Advsum <sumekah33@gmail.com>
2025-05-15 00:08:44 +0800

    feat: Add initial employee creation page with slider transition

---
+++ src/app/(dashboards-management-systems)/Dashboards/threehighplus/createemployee/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import SliderInOut from "@/components/reusable/sliderinout";
+export default function Page() {
+  return (
+    <SliderInOut>
+      <section>Page</section>
+    </SliderInOut>
+  );
+}
