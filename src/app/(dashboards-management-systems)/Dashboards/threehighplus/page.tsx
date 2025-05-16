c80da8b557b2367b355ff01d7e5b6613f966ba95
Advsum <sumekah33@gmail.com>
2025-05-16 12:17:16 +0800

    feat: Enhance layout and page structure with motion effects and improved styling

--- src/app/(dashboards-management-systems)/Dashboards/threehighplus/page.tsx
+++ src/app/(dashboards-management-systems)/Dashboards/threehighplus/page.tsx
@@ -1,15 +1,28 @@
+"use client";
 import React from "react";
-import CreateFormEmployee from '../../allcomponents/threehighplus/createComponents/createEmployeement'
+import CreateFormEmployee from "../../allcomponents/threehighplus/createComponents/createEmployeement";
+import { motion } from "framer-motion";
+
 export default function Page() {
   return (
-    <main className="mt-12 lg:-pl-15 lg:-pr-3 h-auto w-full">
+    <main className="mt-12 lg:-pl-15 lg:-pr-3 h-screen w-full">
       <section>
         <CreateFormEmployee />
       </section>
-
-
-
-
+      <section className=" mt-20 mb-20" />
+      <motion.section
+        initial={{ opacity: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="pt-[1px] pb-[1px] pl-[1px] pr-[1px] bg-gradient-to-br from-[#030E36]/120 to-slate-200 w-1/3 rounded-md shadow-md"
+      >
+        <div className="bg-slate-900 h-full w-full rounded-md">
+          <div className="p-20 text-slate-50 h-[250px]"></div>
+        </div>{" "}
+      </motion.section>{" "}
+      *
       {/* <section className="grid grid-cols-6 grid-rows-9 gap-3 w-full h-[92vh]">
         <div className="col-span-2 row-span-3 col-start-1 row-start-1 bg-slate-800 border-[1px] border-slate-50/20 rounded-md p-2 text-slate-50">
           <h1>Date</h1>
--- src/app/(dashboards-management-systems)/layout.tsx
+++ src/app/(dashboards-management-systems)/layout.tsx
@@ -19,14 +19,14 @@ export default function MyPageLayout({
 }>) {
   return (
     <main className={`${lato.className} flex flex-row w-full bg-slate-900`}>
-      <section className="fixed z-20 shadow-md lg:block hidden">
+      <section className="fixed z-20 shadow-md lg:block hidden min-h-screen">
         <Sidebar />
       </section>
       <section className="w-full lg:pl-[20px] min-h-screen flex flex-col ">
         <div className="lg:pl-14 lg:pr-14 p-1 w-full mt-2 fixed">
           <div className="p-5 border border-slate-50/10 rounded-md bg-slate-800 text-slate-50 z-30">Navigation</div>
         </div>
-        <div className="mt-10 lg:pl-14 lg:pr-10">{children}</div>
+        <div className="mt-10 lg:pl-14 lg:pr-10 h-screen">{children}</div>
       </section>
     </main>
   );
